7bf81edafa3cf786b4961ef3808fd230b0419cc1
Lucas <lucasdgonzalez@outlook.com>
2024-05-06 21:59:39 -0300

    Actualiza el código para una mejor gestión y orden

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import { validarPokemonABuscar } from "./validaciones/validaciones.js";
-import { buscarPokemonEspecifico, hacerSolicitud } from "./api/pokedex.js";
-import { guardarListadoPokemonesEnLocalStorage, guardarDataPokemonEnLocalStorage, cargarListadoPokemonesDeLocalStorage, cargarDataPokemonDeLocalStorage } from "./storage/pokedex.js";
+import { 
+  gestionarListadoPokemones, 
+  gestionarPedidoDataPokemonEspecifico, 
+  gestionarCambioPaginaSiguiente, 
+  gestionarCambioAnteriorPagina, 
+  gestionarActualizacionPagina 
+} from "./gestores/pokedex.js";
+import { mostrarErrorValidacion, imprimirErrorValidacionBuscador } from "./ui/general.js";
 
 // Variables globales
 
@@ -9,207 +15,6 @@ const $nombresPokemon = document.querySelectorAll(".nombre-pokemon-listado");
 const $indicadoresPagina = document.querySelectorAll(".indicador-pagina"); // Indicadores de página del paginador
 const $botonAnteriorPagina = document.querySelector(".boton-anterior-pagina"); // Botón página anterior del paginador
 const $botonSiguientePagina = document.querySelector(".boton-siguiente-pagina"); // Botón página siguiente del paginador
-
-// UI
-
-function imprimirNombresPokemon(nombresPokemon) {
-  $nombresPokemon.forEach(($botonNombrePokemon, i) => {
-    const nombrePokemon = nombresPokemon[i];
-    $botonNombrePokemon.textContent = nombrePokemon;
-  });
-}
-
-function imprimirInformacionPokemonEspecifico(infoPokemon) {
-  imprimirNombrePokemonElegido(infoPokemon);
-  imprimirIdPokemonElegido(infoPokemon);
-  mostrarImagenPokemonElegido(infoPokemon);
-  imprimirTiposPokemonElegido(infoPokemon);
-  imprimirStatsPokemonElegido(infoPokemon);
-  imprimirHabilidadesPokemon(infoPokemon);
-  mostrarInformacionPokemon();
-}
-
-function imprimirNombrePokemonElegido(infoPokemon) {
-  const $nombrePokemon = document.querySelector(".nombre-pokemon");
-  $nombrePokemon.textContent = infoPokemon["nombre"];
-}
-
-function imprimirIdPokemonElegido(infoPokemon) {
-  const $idPokemon = document.querySelector(".id-pokemon");
-  $idPokemon.textContent = `#${infoPokemon["id"]}`;
-}
-
-function mostrarImagenPokemonElegido(infoPokemon) {
-  const $imagenPokemon = document.querySelector("#imagen-pokemon");
-  $imagenPokemon.src = infoPokemon["imagenPokemon"];
-}
-
-function imprimirTiposPokemonElegido(infoPokemon) {
-  const $tiposPokemonElegido = document.querySelectorAll(".imagen-tipos-pokemon");
-  const tiposPokemonElegido = infoPokemon["tipos"];
-
-  tiposPokemonElegido.forEach((tipoPokemon, i) => {
-    $tiposPokemonElegido[i].src = `img/tipos/${tipoPokemon}.svg`;
-  });
-
-  if (tiposPokemonElegido.length === 1) {
-    $tiposPokemonElegido[1].id = "oculto";
-  } else {
-    $tiposPokemonElegido[1].id = "";
-  }
-}
-
-function imprimirStatsPokemonElegido(infoPokemon) {
-  const $vidaBasePokemon = document.querySelector(".vida-base-respuesta");
-  const $ataqueBasePokemon = document.querySelector(".ataque-base-respuesta");
-  const $defensaBasePokemon = document.querySelector(".defensa-base-respuesta");
-  const $ataqueEspecialPokemon = document.querySelector(".ataque-especial-base-respuesta");
-  const $defensaEspecialPokemon = document.querySelector(".defensa-especial-base-respuesta");
-  const $velocidadBasePokemon = document.querySelector(".velocidad-base-respuesta");
-
-  const vidaBasePokemon = infoPokemon["stats"]["hp"];
-  const ataqueBasePokemon = infoPokemon["stats"]["attack"];
-  const defensaBasePokemon = infoPokemon["stats"]["defense"];
-  const ataqueEspecialBasePokemon = infoPokemon["stats"]["special-attack"];
-  const defensaEspecialBasePokemon = infoPokemon["stats"]["special-defense"];
-  const velocidadBasePokemon = infoPokemon["stats"]["speed"];
-
-  $vidaBasePokemon.textContent = vidaBasePokemon;
-  $ataqueBasePokemon.textContent = ataqueBasePokemon;
-  $defensaBasePokemon.textContent = defensaBasePokemon;
-  $ataqueEspecialPokemon.textContent = ataqueEspecialBasePokemon;
-  $defensaEspecialPokemon.textContent = defensaEspecialBasePokemon;
-  $velocidadBasePokemon.textContent = velocidadBasePokemon;
-}
-
-function imprimirHabilidadesPokemon(infoPokemon) {
-  const $contenedorHabilidadesPokemon = document.querySelector(".lista-habilidades");
-
-  while ($contenedorHabilidadesPokemon.firstChild) {
-    $contenedorHabilidadesPokemon.removeChild($contenedorHabilidadesPokemon.firstChild);
-  }
-
-  const cantidadHabilidadesPokemon = (infoPokemon["habilidades"]).length;
-  crearItemsListaHabilidades(cantidadHabilidadesPokemon);
-
-  const $habilidadesPokemon = document.querySelectorAll(".item-lista-habilidades");
-  const habilidadesPokemon = infoPokemon["habilidades"];
-
-  habilidadesPokemon.forEach((habilidadPokemon, i) => {
-    $habilidadesPokemon[i].textContent = habilidadPokemon;
-  })
-}
-
-function crearItemsListaHabilidades(cantidadHabilidades) {
-  const $listaHabilidadesPokemon = document.querySelector(".lista-habilidades");
-
-  for (let i = 0; i < cantidadHabilidades; i++) {
-    const itemListaHabilidades = document.createElement("li");
-    itemListaHabilidades.className = "habilidad-pokemon item-lista-habilidades";
-    $listaHabilidadesPokemon.appendChild(itemListaHabilidades);
-  }
-}
-
-function mostrarInformacionPokemon() {
-  const $bloqueInformacionPokemon = document.querySelector(".contenedor-informacion-pokemon");
-  $bloqueInformacionPokemon.id = "";
-}
-
-function actualizarNumerosIndicadorPagina(accionar, paginaSolicitada, $indicadoresPagina) {
-  let accionesPaginador = ["anterior", "siguiente", "especifico"];
-
-  if (accionar === accionesPaginador[0]) {
-    let numeroAImprimir = paginaSolicitada;
-    $indicadoresPagina.forEach(($indicador) => {
-      $indicador.textContent = numeroAImprimir;
-      numeroAImprimir++;
-    });
-  } else if (accionar === accionesPaginador[1]) {
-    let numeroAImprimir = paginaSolicitada + 2;
-    $indicadoresPagina.forEach(($indicador) => {
-      $indicador.textContent = numeroAImprimir;
-      numeroAImprimir++;
-    });
-  } else if (accionar === accionesPaginador[2]) {
-    let numeroAImprimir = paginaSolicitada + 1;
-    $indicadoresPagina.forEach(($indicador) => {
-      $indicador.textContent = numeroAImprimir;
-      numeroAImprimir++;
-    });
-  } else {
-    return false;
-  }
-}
-
-function desactivarPaginaActiva() {
-  const $contenedoresNumerosPagina = document.querySelectorAll(".pagina-item");
-
-  $contenedoresNumerosPagina.forEach(($contenedorNumeroPagina) => {
-    $contenedorNumeroPagina.classList.remove("active");
-    const $enlaceNumeroPagina = $contenedorNumeroPagina.querySelector(".indicador-pagina");
-    $enlaceNumeroPagina.classList.remove("active");
-  });
-}
-
-function mostrarPaginaActiva(paginaParaActivar, $indicadoresPagina) {
-  const numeroPaginaACambiar = paginaParaActivar + 1;
-
-  $indicadoresPagina.forEach(($indicadorPagina) => {
-    const numeroIndicador = Number($indicadorPagina.textContent);
-
-    if (numeroIndicador === numeroPaginaACambiar) {
-      $indicadorPagina.classList.add("active");
-    }
-  });
-}
-
-function desactivarBotonAnteriorPagina() {
-  const $estadoBotonAnterior = document.querySelector(".indicador-estado-anterior");
-  $estadoBotonAnterior.classList.add("disabled");
-
-  const $botonAnteriorPagina = $estadoBotonAnterior.querySelector(".boton-anterior-pagina");
-  $botonAnteriorPagina.classList.add("disabled");
-}
-
-function activarBotonAnteriorPagina() {
-  const $estadoBotonAnterior = document.querySelector(".indicador-estado-anterior");
-  $estadoBotonAnterior.classList.remove("disabled");
-
-  const $botonAnteriorPagina = $estadoBotonAnterior.querySelector(".boton-anterior-pagina");
-  $botonAnteriorPagina.classList.remove("disabled");
-}
-
-function desactivarBotonSiguientePagina() {
-  const $estadoBotonSiguiente = document.querySelector(".indicador-estado-siguiente");
-  $estadoBotonSiguiente.classList.add("disabled");
-
-  const $botonSiguientePagina = $estadoBotonSiguiente.querySelector(".boton-siguiente-pagina");
-  $botonSiguientePagina.classList.add("disabled");
-}
-
-function activarBotonSiguientePagina() {
-  const $estadoBotonSiguiente = document.querySelector(".indicador-estado-siguiente");
-  $estadoBotonSiguiente.classList.remove("disabled");
-
-  const $botonSiguientePagina = $estadoBotonSiguiente.querySelector(".boton-siguiente-pagina");
-  $botonSiguientePagina.classList.remove("disabled");
-}
-
-function imprimirErrorValidacionBuscador(error) {
-  const $contenedorErrorValidacion = document.querySelector(".contenedor-error-validacion");
-  const textoErrorValidacion = $contenedorErrorValidacion.querySelector(".error-validacion");
-  textoErrorValidacion.textContent = error;
-}
-
-function mostrarErrorValidacion() {
-  const $contenedorErrorValidacion = document.querySelector(".contenedor-error-validacion");
-  $contenedorErrorValidacion.id = "";
-}
-
-function ocultarErrorValidacion() {
-  const $contenedorErrorValidacion = document.querySelector(".contenedor-error-validacion");
-  $contenedorErrorValidacion.id = "oculto";
-}
 
 // Inicio página
 
@@ -220,12 +25,33 @@ iniciarPagina();
 
 // DOM
 
+/* Buscador */
+
+const $botonBuscarPokemon = document.querySelector(".boton-buscar-pokemon");
+
+$botonBuscarPokemon.addEventListener("click", () => {
+  const pokemonABuscar = document.querySelector(".buscador-pokemon").value;
+
+  const error = validarPokemonABuscar(pokemonABuscar);
+
+  if (error !== "") {
+    mostrarErrorValidacion();
+    imprimirErrorValidacionBuscador(error);
+  } else {
+    gestionarPedidoDataPokemonEspecifico(pokemonABuscar);
+  }
+})
+
+/* Listado pokemones */
+
 $nombresPokemon.forEach(($nombrePokemon) => {
   $nombrePokemon.addEventListener("click", () => {
     const nombrePokemon = $nombrePokemon.textContent;
-    gestionarBusquedaPokemonEspecifico(nombrePokemon);
+    gestionarPedidoDataPokemonEspecifico(nombrePokemon);
   })
 })
+
+/* Modal */
 
 $indicadoresPagina.forEach(($indicadorPagina) => {
   $indicadorPagina.addEventListener("click", () => {
@@ -242,181 +68,4 @@ $botonAnteriorPagina.addEventListener("click", () => {
 $botonSiguientePagina.addEventListener("click", () => {
   const numeroPaginaActual = Number(document.querySelector(".active").textContent);
   gestionarCambioPaginaSiguiente(numeroPaginaActual, $indicadoresPagina);
-})
-
-// Generales
-
-async function gestionarBusquedaPokemonEspecifico(identificadorPokemon) {
-  const identificadorFinal = identificadorPokemon.toLowerCase();
-
-  try {
-    const pokemonGuardado = cargarDataPokemonDeLocalStorage(identificadorFinal);
-    ocultarErrorValidacion();
-    imprimirInformacionPokemonEspecifico(pokemonGuardado);
-  } catch (error) {
-    gestionarBusquedaDataPokemonEspecifico(identificadorFinal);
-  }
-}
-
-async function gestionarBusquedaDataPokemonEspecifico(identificadorPokemon) {
-  try {
-    const dataPokemon = await buscarPokemonEspecifico(identificadorPokemon);
-    const infoPokemon = dividirInformacionPokemon(dataPokemon);
-    guardarDataPokemonEnLocalStorage(infoPokemon);
-    ocultarErrorValidacion();
-    imprimirInformacionPokemonEspecifico(infoPokemon);
-  } catch (error) {
-    mostrarErrorValidacion();
-    imprimirErrorValidacionBuscador(error);
-  }
-}
-
-function dividirInformacionPokemon(dataPokemon) {
-  const infoPokemon = {
-    id: dataPokemon["id"],
-    nombre: dataPokemon["name"],
-    tipos: [],
-    stats: [],
-    imagenPokemon: dataPokemon["sprites"]["front_default"],
-    habilidades: []
-  }
-
-  const cantidadTiposPokemon = dataPokemon["types"];
-
-  cantidadTiposPokemon.forEach((tipoPokemon) => {
-    const nombreTipo = tipoPokemon["type"]["name"];
-    infoPokemon["tipos"].push(nombreTipo);
-  })
-
-  const statsPokemon = dataPokemon["stats"];
-
-  statsPokemon.forEach((statPokemon) => {
-    const nombreStat = statPokemon["stat"]["name"];
-    const valorStat = statPokemon["base_stat"];
-
-    infoPokemon["stats"][nombreStat] = valorStat;
-  })
-
-  const habilidadesPokemon = dataPokemon["abilities"];
-
-  habilidadesPokemon.forEach((habilidadPokemon) => {
-    const nombreHabilidad = habilidadPokemon["ability"]["name"];
-    infoPokemon["habilidades"].push(nombreHabilidad);
-  })
-
-  return infoPokemon;
-}
-
-function gestionarCambioPaginaSiguiente(numeroPaginaActual, $indicadoresPagina) {
-  let indicadorPagina = numeroPaginaActual - 1;
-  let numeroPaginaSolicitada = numeroPaginaActual;
-  const limitePaginas = 62;
-  const accionar = "siguiente";
-  const indicadorDefinitivo = calcularNumeroPokemonListado(numeroPaginaSolicitada);
-
-  if (indicadorPagina === limitePaginas) {
-    return false;
-  } else if (indicadorPagina === limitePaginas - 1) {
-    desactivarBotonSiguientePagina();
-    desactivarPaginaActiva();
-    actualizarNumerosIndicadorPagina(accionar, indicadorPagina, $indicadoresPagina);
-    mostrarPaginaActiva(numeroPaginaSolicitada, $indicadoresPagina);
-    gestionarListadoPokemones(indicadorDefinitivo);
-  } else {
-    activarBotonAnteriorPagina();
-    desactivarPaginaActiva();
-    actualizarNumerosIndicadorPagina(accionar, indicadorPagina, $indicadoresPagina);
-    mostrarPaginaActiva(numeroPaginaSolicitada, $indicadoresPagina);
-    gestionarListadoPokemones(indicadorDefinitivo);
-  }
-}
-
-function gestionarCambioAnteriorPagina(numeroPaginaActual, $indicadoresPagina) {
-  let indicadorPagina = numeroPaginaActual - 1;
-  let numeroPaginaSolicitada = numeroPaginaActual - 2;
-  const accionar = "anterior";
-  const indicadorDefinitivo = calcularNumeroPokemonListado(numeroPaginaSolicitada);
-
-  if (indicadorPagina === 0) {
-    return false;
-  } else if (indicadorPagina === 1) {
-    actualizarNumerosIndicadorPagina(accionar, indicadorPagina, $indicadoresPagina);
-    desactivarPaginaActiva();
-    mostrarPaginaActiva(numeroPaginaSolicitada, $indicadoresPagina);
-    calcularNumeroPokemonListado(indicadorPagina);
-    desactivarBotonAnteriorPagina();
-    gestionarListadoPokemones(indicadorDefinitivo);
-  } else {
-    activarBotonSiguientePagina();
-    desactivarPaginaActiva();
-    actualizarNumerosIndicadorPagina(accionar, indicadorPagina, $indicadoresPagina);
-    mostrarPaginaActiva(numeroPaginaSolicitada, $indicadoresPagina);
-    gestionarListadoPokemones(indicadorDefinitivo);
-  }
-}
-
-function gestionarActualizacionPagina(numeroPaginaSolicitada, $indicadoresPagina) {
-  const indicadorPaginaASolicitar = numeroPaginaSolicitada - 1;
-  const ACCIONAR = "especifico";
-
-  const indicadorDefinitivo = calcularNumeroPokemonListado(indicadorPaginaASolicitar);
-
-  actualizarNumerosIndicadorPagina(ACCIONAR, indicadorPaginaASolicitar, $indicadoresPagina);
-  desactivarPaginaActiva();
-  mostrarPaginaActiva(indicadorPaginaASolicitar, $indicadoresPagina);
-  activarBotonAnteriorPagina();
-  gestionarListadoPokemones(indicadorDefinitivo);
-}
-
-function calcularNumeroPokemonListado(indicadorPaginaASolicitar) {
-  const POKEMONES_POR_PAGINA = 9;
-  const resultado = (POKEMONES_POR_PAGINA * indicadorPaginaASolicitar);
-
-  return resultado;
-}
-
-async function gestionarListadoPokemones(indicadorPagina) {
-  const paginaActual = Number(document.querySelector(".active").textContent);
-  const LIMITE_POKEMONES = 9;
-
-  try {
-    const dataPokemon = cargarListadoPokemonesDeLocalStorage(paginaActual);
-    let nombresPokemon = [];
-
-    dataPokemon["results"].forEach(pokemon => {
-      const nombrePokemon = pokemon.name;
-      nombresPokemon.push(nombrePokemon);
-    });
-
-    imprimirNombresPokemon(nombresPokemon);
-  } catch (error) {
-    const dataPokemon = await hacerSolicitud(indicadorPagina, LIMITE_POKEMONES);
-    guardarListadoPokemonesEnLocalStorage(paginaActual, dataPokemon);
-
-    let nombresPokemon = [];
-
-    dataPokemon["results"].forEach(pokemon => {
-      const nombrePokemon = pokemon.name;
-      nombresPokemon.push(nombrePokemon);
-    });
-
-    imprimirNombresPokemon(nombresPokemon);
-  }
-}
-
-/* Buscador */
-
-const $botonBuscarPokemon = document.querySelector(".boton-buscar-pokemon");
-
-$botonBuscarPokemon.addEventListener("click", () => {
-  const pokemonABuscar = document.querySelector(".buscador-pokemon").value;
-
-  const error = validarPokemonABuscar(pokemonABuscar);
-
-  if (error !== "") {
-    mostrarErrorValidacion();
-    imprimirErrorValidacionBuscador(error);
-  } else {
-    gestionarBusquedaPokemonEspecifico(pokemonABuscar);
-  }
 })
