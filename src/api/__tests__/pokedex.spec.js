6905a281edd6bad6408782a9621e1fe2f788fdca
Lucas <lucasdgonzalez@outlook.com>
2024-05-26 20:36:57 -0300

    Sube prueba que devuelve un error al no encontrar listado

--- src/api/__tests__/pokedex.spec.js
+++ src/api/__tests__/pokedex.spec.js
@@ -5,18 +5,32 @@ import fixturePrimeraListaPokemones from "../../../cypress/fixtures/listado-pagi
 import fixtureCharmander from "../../../cypress/fixtures/charmander.json";
 
 describe("hacerSolicitud", () => {
-  it("Devuelve la data de la primera lista de pokemones correctamente", async () => {
+  beforeEach(() => {
     global.fetch = jest.fn(() =>
       Promise.resolve({
         json: () => Promise.resolve(fixturePrimeraListaPokemones)
       })
     );
+  });
 
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it("Devuelve la data de la primera lista de pokemones correctamente", async () => {
     const INDICADOR_POKEMON = 0;
     const LIMITE_POKEMONES = 9;
 
     const data = await hacerSolicitud(INDICADOR_POKEMON, LIMITE_POKEMONES);
 
     expect(data).toEqual(fixturePrimeraListaPokemones);
+  });
+
+  it("Devuelve un error al no pasarle un identificador a la función", async () => {
+    fetch.mockImplementationOnce(() =>
+      Promise.reject(new Error("No se encontró el listado de pokemones indicado"))
+    );
+
+    await expect(hacerSolicitud("", "")).rejects.toThrow("No se encontró el listado de pokemones indicado");
   });
 });
