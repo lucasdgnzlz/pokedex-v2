99f22561ff40d5e8ca6ee8ed1799cfd2cf69a1c2
Lucas <lucasdgonzalez@outlook.com>
2024-05-28 20:53:33 -0300

    Sube test que devuelve la data de la API dividida

--- src/utilidades/__tests__/utilidades.spec.js
+++ src/utilidades/__tests__/utilidades.spec.js
@@ -11,3 +11,18 @@ describe("calcularNumeroPokemonListado", () => {
     expect(calcularNumeroPokemonListado(INDICADOR_PAGINA)).toEqual(resultadoEsperado);
   });
 });
+
+describe("dividirInformacionPokemon", () => {
+  it("Deuvelve la data de la API dividida para dejar únicamente lo necesario", () => {
+    const dataEsperada = {
+      id: 4,
+      nombre: "charmander",
+      tipos: ["fire"],
+      stats: [{"hp": 39}, {"attack": 52}, {"defense": 43}, {"special-attack": 60}, {"special-defense": 50}, {"speed": 65}],
+      imagenPokemon: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+      habilidades: ["blaze", "solar-power"]
+    };
+
+    expect(dividirInformacionPokemon(fixtureCharmander)).toEqual(dataEsperada);
+  });
+});
