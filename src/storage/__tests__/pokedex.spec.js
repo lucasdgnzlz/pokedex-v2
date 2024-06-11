fb31f06bdd647c0504c0f8f5c02d1449a0c5d5d5
Lucas <lucasdgonzalez@outlook.com>
2024-06-10 22:02:10 -0300

    Devuelve un error si el indicadorPagina no es type number

---
+++ src/storage/__tests__/pokedex.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="Jest" />
+
+import { guardarListadoPokemonesEnLocalStorage, cargarListadoPokemonesDeLocalStorage, guardarDataPokemonEnLocalStorage, cargarDataPokemonDeLocalStorage } from "../pokedex.js";
+import { dividirInformacionPokemon } from "../../utilidades/utilidades.js";
+import fixtureCharmander from "../../../cypress/fixtures/charmander.json";
+import fixturePrimeraListaPokemones from "../../../cypress/fixtures/listado-pagina-1.json";
+
+describe("guardarListadoPokemonesEnLocalStorage", () => {
+  it("Devuelve un error si el indicadorPagina no es type number", () => {
+    const INDICADOR_PAGINA = "indicadorPaginaNoEsUnNumero";
+    const dataPokemon = fixturePrimeraListaPokemones;
+
+    expect(() => { guardarListadoPokemonesEnLocalStorage(INDICADOR_PAGINA, dataPokemon)}).toThrow("Se necesita el número de la página a la que pertenece la data, y los pokemones para guardar en el localStorage");
+  });
+});
