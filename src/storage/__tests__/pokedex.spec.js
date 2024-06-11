4e0d19f6c066139ccbe071df7d1382b4adea7530
Lucas <lucasdgonzalez@outlook.com>
2024-06-11 20:50:29 -0300

    Prueba guardar la data en localStorage

--- src/storage/__tests__/pokedex.spec.js
+++ src/storage/__tests__/pokedex.spec.js
@@ -1,4 +1,5 @@
 /// <reference types="Jest" />
+import { expect, jest, test } from '@jest/globals';
 
 import { guardarListadoPokemonesEnLocalStorage, cargarListadoPokemonesDeLocalStorage, guardarDataPokemonEnLocalStorage, cargarDataPokemonDeLocalStorage } from "../pokedex.js";
 import { dividirInformacionPokemon } from "../../utilidades/utilidades.js";
@@ -10,13 +11,27 @@ describe("guardarListadoPokemonesEnLocalStorage", () => {
     const INDICADOR_PAGINA = "indicadorPaginaNoEsUnNumero";
     const dataPokemon = fixturePrimeraListaPokemones;
 
-    expect(() => { guardarListadoPokemonesEnLocalStorage(INDICADOR_PAGINA, dataPokemon)}).toThrow("Se necesita el número de la página a la que pertenece la data, y los pokemones para guardar en el localStorage");
+    expect(() => { guardarListadoPokemonesEnLocalStorage(INDICADOR_PAGINA, dataPokemon) }).toThrow("Se necesita el número de la página a la que pertenece la data, y los pokemones para guardar en el localStorage");
   });
 
   it("Devuelve un error si la dataPokemon no es un objeto", () => {
     const INDICADOR_PAGINA = 1;
     const dataPokemon = "dataPokemon no es un objeto";
 
-    expect(() => { guardarListadoPokemonesEnLocalStorage(INDICADOR_PAGINA, dataPokemon)}).toThrow("Se necesita el número de la página a la que pertenece la data, y los pokemones para guardar en el localStorage");
+    expect(() => { guardarListadoPokemonesEnLocalStorage(INDICADOR_PAGINA, dataPokemon) }).toThrow("Se necesita el número de la página a la que pertenece la data, y los pokemones para guardar en el localStorage");
+  });
+
+  it("Guarda la data en localStorage", () => {
+    const INDICADOR_PAGINA = 1;
+    const pokemons = fixturePrimeraListaPokemones;
+
+    const setItemMockeado = jest.spyOn(Storage.prototype, 'setItem');
+
+    guardarListadoPokemonesEnLocalStorage(INDICADOR_PAGINA, pokemons);
+
+    expect(setItemMockeado).toHaveBeenCalledWith(`pagina_${INDICADOR_PAGINA}`, JSON.stringify(pokemons));
+
+    setItemMockeado.mockRestore(); // Restaura los mocks
+    localStorage.clear(); // Limpia el storage
   });
 });
