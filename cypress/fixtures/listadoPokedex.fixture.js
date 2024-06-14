f5953dd6c7321491f8cb54765a871f15c4188c1d
Lucas <lucasdgonzalez@outlook.com>
2024-06-13 21:35:16 -0300

    Sube nuevo fixture del listado pokemon y el paginador

---
+++ cypress/fixtures/listadoPokedex.fixture.js
@@ -0,0 +1,28 @@
+export default `
+        <div class="contenedor-listado-pokemones">
+          <p class="nombre-pokemon-listado"></p>
+          <p class="nombre-pokemon-listado"></p>
+          <p class="nombre-pokemon-listado"></p>
+          <p class="nombre-pokemon-listado"></p>
+          <p class="nombre-pokemon-listado"></p>
+          <p class="nombre-pokemon-listado"></p>
+          <p class="nombre-pokemon-listado"></p>
+          <p class="nombre-pokemon-listado"></p>
+          <p class="nombre-pokemon-listado"></p>
+        </div>
+        
+        <ul class="pagination paginador">
+          <li class="page-item indicador-estado-anterior disabled">
+            <a class="page-link boton-anterior-pagina" aria-label="Previous">
+              <span aria-hidden="true">&laquo;</span>
+            </a>
+          </li>
+          <li class="page-item pagina-item active"><a class="page-link indicador-pagina">1</a></li>
+          <li class="page-item pagina-item"><a class="page-link indicador-pagina">2</a></li>
+          <li class="page-item pagina-item"><a class="page-link indicador-pagina">3</a></li>
+          <li class="page-item indicador-estado-siguiente">
+            <a class="page-link boton-siguiente-pagina" aria-label="Next">
+              <span aria-hidden="true">&raquo;</span>
+            </a>
+          </li>
+        </ul>`;
