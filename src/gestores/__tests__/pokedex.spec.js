d44cefc5a9e544ae1f2d165eecbae378f0433152
Lucas <lucasdgonzalez@outlook.com>
2024-06-24 21:13:15 -0300

    Gestiona impresión de info de pkmn especifico por la API

--- src/gestores/__tests__/pokedex.spec.js
+++ src/gestores/__tests__/pokedex.spec.js
@@ -1,13 +1,16 @@
 /// <reference types="Jest" />
 
 import { gestionarListadoPokemones, gestionarPedidoDataPokemonEspecifico, gestionarCambioPaginaSiguiente, gestionarCambioAnteriorPagina, gestionarActualizacionPagina } from "../pokedex.js";
-import fixturePrimeraListaPokemones from "../../../cypress/fixtures/listado-pagina-1.json";
-import fixtureListadoPokemonesYPaginador from "../../../cypress/fixtures/listadoPokedex.fixture.js";
 import { guardarListadoPokemonesEnLocalStorage } from "../../storage/pokedex.js";
+import fixturePrimeraListaPokemones from "../../../cypress/fixtures/listado-pagina-1.json";
+import fixtureDataCharmander from "../../../cypress/fixtures/charmander.json";
+import fixtureListadoPokemonesYPaginador from "../../../cypress/fixtures/listadoPokedex.fixture.js";
+import infoPokemonEspecificoFixture from "../../../cypress/fixtures/infoPokemonEspecifico.fixture.js";
 
 describe("gestionarListadoPokemones", () => {
   afterEach(() => {
     jest.resetAllMocks();
+    document.body.innerHTML = ''; // Limpiar el DOM
   });
 
   it("Gestiona impresión del listado de pokemones directamente llamando a la API", async () => {
@@ -39,9 +42,34 @@ describe("gestionarListadoPokemones", () => {
 
     await gestionarListadoPokemones(INDICADOR_PAGINA);
 
-    const pokemonesEnListado = document.querySelectorAll(".nombre-pokemon-listado");
-    pokemonesEnListado.forEach((pokemonLista, i) => {
+    const $pokemonesEnListado = document.querySelectorAll(".nombre-pokemon-listado");
+    $pokemonesEnListado.forEach((pokemonLista, i) => {
       expect(pokemonLista.textContent).toEqual(fixturePrimeraListaPokemones["results"][i]["name"]);
     });
+  });
+});
+
+describe("gestionarPedidoDataPokemonEspecifico", () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+    document.body.innerHTML = ''; // Limpiar el DOM
+  });
+
+  it("Gestiona impresión de info de un pkmn específico mediante una llamada a la API", async () => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(fixtureDataCharmander)
+      })
+    );
+
+    document.body.innerHTML = infoPokemonEspecificoFixture;
+
+    const $nombreInicialPokemon = document.querySelector(".nombre-pokemon");
+    expect($nombreInicialPokemon.textContent).toEqual("");
+
+    const IDENTIFICADOR_POKEMON = "charmander";
+    await gestionarPedidoDataPokemonEspecifico(IDENTIFICADOR_POKEMON);
+
+    expect($nombreInicialPokemon.textContent).toEqual(fixtureDataCharmander.name);
   });
 });
