b12702128ead12868898d4a06a736ddab5a9bbfb
Lucas <lucasdgonzalez@outlook.com>
2024-06-27 21:49:30 -0300

    Cambia el listado de la página actual a la última

--- src/gestores/__tests__/pokedex.spec.js
+++ src/gestores/__tests__/pokedex.spec.js
@@ -5,6 +5,7 @@ import { guardarListadoPokemonesEnLocalStorage, guardarDataPokemonEnLocalStorage
 import { dividirInformacionPokemon } from "../../utilidades/utilidades.js";
 import fixturePrimeraListaPokemones from "../../../cypress/fixtures/listado-pagina-1.json";
 import fixtureSegundaListaPokemones from "../../../cypress/fixtures/listado-pagina-2.json";
+import fixtureUltimaListaPokemones from "../../../cypress/fixtures/listado-ultima-pagina.json";
 import fixtureDataCharmander from "../../../cypress/fixtures/charmander.json";
 import fixtureListadoPokemonesYPaginador from "../../../cypress/fixtures/listadoPokedex.fixture.js";
 import infoPokemonEspecificoFixture from "../../../cypress/fixtures/infoPokemonEspecifico.fixture.js";
@@ -112,6 +113,11 @@ describe("gestionarPedidoDataPokemonEspecifico", () => {
 });
 
 describe("gestionarCambioPaginaSiguiente", () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+    document.body.innerHTML = ''; // Limpiar el DOM
+  });
+
   it("Cambia el listado de la página actual a la siguiente", () => {
     global.fetch = jest.fn(() =>
       Promise.resolve({
@@ -127,6 +133,27 @@ describe("gestionarCambioPaginaSiguiente", () => {
     gestionarCambioPaginaSiguiente(INDICADOR_PAGINA_SOLICITADA, $indicadoresPagina);
 
     const respuestaEsperada = ["2", "3", "4"];
+    $indicadoresPagina.forEach((indicadorPagina, i) => {
+      expect(indicadorPagina.textContent).toEqual(respuestaEsperada[i]);
+    });
+  });
+
+  it("Cambia el listado de la página actual a la última", () => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(fixtureUltimaListaPokemones)
+      })
+    );
+
+    document.body.innerHTML = fixtureListadoPokemonesYPaginador;
+
+    const INDICADOR_PAGINA_ACTUAL = 113;
+
+    const $indicadoresPagina = document.querySelectorAll(".pagina-item");
+
+    gestionarCambioPaginaSiguiente(INDICADOR_PAGINA_ACTUAL, $indicadoresPagina);
+
+    const respuestaEsperada = ["114", "115", "116"];
     $indicadoresPagina.forEach((indicadorPagina, i) => {
       expect(indicadorPagina.textContent).toEqual(respuestaEsperada[i]);
     });
